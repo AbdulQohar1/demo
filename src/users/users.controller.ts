4a797ef8a527d58e111b6333a9150778ce568835
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2024-10-28 23:22:23 +0100

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Crud, CrudController } from '@dataui/crud';
+// import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { from } from 'rxjs';
+
+import {User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Crud({
+  model: {
+    type: User
+  },
+})
+@Controller('users')
+export class UsersController implements CrudController<User> {
+  constructor(public service: UsersService) {}
+}
+// export class UsersController {
+//   constructor(private readonly usersService: UsersService) {}
+
+//   @Post()
+//   create(@Body() createUserDto: CreateUserDto) {
+//     return this.usersService.create(createUserDto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.usersService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.usersService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+//     return this.usersService.update(+id, updateUserDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.usersService.remove(+id);
+//   }
+// }
---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmCrudService } from '@dataui/crud-typeorm';
+import { User } from '../users/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService extends TypeOrmCrudService<User> {
+  constructor(@InjectRepository(User) repo) {
+    super(repo)
+  }
+}
+// export class UsersService {
+//   create(createUserDto: CreateUserDto) {
+//     return 'This action adds a new user';
+//   }
+
+//   findAll() {
+//     return `This action returns all users`;
+//   }
+
+//   findOne(id: number) {
+//     return `This action returns a #${id} user`;
+//   }
+
+//   update(id: number, updateUserDto: UpdateUserDto) {
+//     return `This action updates a #${id} user`;
+//   }
+
+//   remove(id: number) {
+//     return `This action removes a #${id} user`;
+//   }
+// }
